d22532b6ee36c71ccbcf80b7c806e19817595691
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-03-16 20:41:11 -0400

    Create object using constructor method

---
+++ JS_design_patterns/constructor_pattern.js
@@ -0,0 +1,10 @@
+class Human {
+  constructor(name, age, occupation) {
+    this.name = name;
+    this.age = age;
+    this.occupation = occupation;
+  }
+}
+
+const person = new Human("John", 22, "Engineer");
+console.log(person)
